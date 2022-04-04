2f671376f685cc53ec3daa994cdd1b94cbfde0cf
Luca <lb.luca.bonetti@gmail.com>
2022-04-04 23:06:39 +0200

    Add Vite configuration alias to okta-auth-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    alias: [
+      {
+        find: "@okta/okta-auth-js",
+        replacement: require.resolve(
+          "@okta/okta-auth-js/dist/okta-auth-js.umd.js"
+        ),
+      },
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+});
