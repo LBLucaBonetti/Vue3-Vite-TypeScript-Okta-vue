509ce27a2314bd7df1053316574416a14abcc60a
Luca <lb.luca.bonetti@gmail.com>
2022-04-05 08:34:18 +0200

    Add okta-auth-js missing dependency

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    /*
     alias: [
       {
         find: "@okta/okta-auth-js",
@@ -19,5 +20,9 @@ export default defineConfig({
         replacement: fileURLToPath(new URL("./src", import.meta.url)),
       },
     ],
+    */
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
